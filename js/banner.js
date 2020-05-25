edf994aedb7426436c5798749ca8cdd336217a77
马义雯 <402635876@qq.com>
2020-05-25 18:00:26 +0800

    幻灯片自动切换以及代码优化

--- js/banner.js
+++ js/banner.js
@@ -1,57 +1,84 @@
-let slideWrap=document.getElementById('slide-wrap');
-let slideContent=slideWrap.querySelector('.slide-content');
-let viewWidth=document.documentElement.clientWidth||document.body.clientWidth;
-let firstUl=slideContent.querySelector('.clearfix');
-let liItem=firstUl.children;
-let navItem=slideWrap.querySelector('.slide-nav').getElementsByTagName('a');
-let prev=slideWrap.querySelector('.slide-prev');
-let next=slideWrap.querySelector('.slide-next');
+let slideWrap = document.getElementById('slide-wrap');
+let slideContent = slideWrap.querySelector('.slide-content');
+let viewWidth = document.documentElement.clientWidth || document.body.clientWidth;
+let firstUl = slideContent.querySelector('.clearfix');
+let liItem = firstUl.children;
+let navItem = slideWrap.querySelector('.slide-nav').getElementsByTagName('a');
+let prev = slideWrap.querySelector('.slide-prev'); //下一页按钮
+let next = slideWrap.querySelector('.slide-next'); //上一页按钮
 /* 存储当前幻灯片的索引 */
-let idx=0;
+let idx = 0;
+/* 滚动开关标记，防止频繁点击上下一页按钮幻灯片切换异常 */
+let flag = false;
+/* 定时器，切换幻灯片完成1s钟以后才能进行下一次切换 */
+let timer = null;
+/* 自动播放的定时器 */
+let autoTimer = null;
 /* 设置滑动对象的宽度 */
-slideContent.style.width=viewWidth*liItem.length+'px';
+slideContent.style.width = viewWidth * liItem.length + 'px';
 /* 设置li对象的宽度 */
-for(let i=0;i<liItem.length;i++){
-    liItem[i].style.width=viewWidth+'px';
+for (let i = 0; i < liItem.length; i++) {
+    liItem[i].style.width = viewWidth + 'px';
 }
 /* 给导航a设置点击事件,按钮高光切换 */
-for(let i=0;i<navItem.length;i++){
-    navItem[i].onclick=function(){
-        for(let j=0;j<navItem.length;j++){
-            navItem[j].className='';
-        }
-        this.className='current';
-        idx=i;//存储当前的索引
-        imgSlide(i);
+for (let i = 0; i < navItem.length; i++) {
+    navItem[i].onclick = function () {
+        idx = i; //存储当前的索引
+        imgSlide();
     }
-}
-// 图片滑动
-function imgSlide(idx){
-    let left=idx*viewWidth;
-    slideContent.style.left=-left+'px';
 }
 /* 左右箭头点击事件 */
-prev.onclick=function(){
+prev.onclick = function () {
     // idx--;
-    console.log(idx);
-    if(--idx<0){
-        idx=liItem.length-1;
-    } 
-    for(let j=0;j<navItem.length;j++){
-        navItem[j].className='';
-    }
-    navItem[idx].className='current';
-    imgSlide(idx);
-}
-next.onclick=function(){
+    if (flag) return;
+    flag = true;
+    prevSlide();
+};
+
+next.onclick = function () {
     // idx++;
-    console.log(idx);
-    if(++idx>liItem.length-1){
-        idx=0;
+    if (flag) return;
+    flag = true;
+    nextSlide();
+};
+
+function prevSlide() {
+    if (--idx < 0) {
+        idx = liItem.length - 1;
     }
-    for(let j=0;j<navItem.length;j++){
-        navItem[j].className='';
+    imgSlide();
+}
+
+function nextSlide() {
+    if (++idx > liItem.length - 1) {
+        idx = 0;
     }
-    navItem[idx].className='current';
-    imgSlide(idx);
+    imgSlide();
+}
+/* 执行自动播放 */
+autoPlay();
+/* 自动播放幻灯片 */
+function autoPlay() {
+    autoTimer = setInterval(function () {
+        nextSlide();
+    }, 3000);
+}
+/* 鼠标滑入停止幻灯片播放，滑出继续幻灯片播放 */
+slideWrap.onmouseenter = function () {
+    clearInterval(autoTimer);
+};
+slideWrap.onmouseleave = autoPlay;
+/* 按钮高亮切换，幻灯片切换 */
+function imgSlide() {
+    for (let j = 0; j < navItem.length; j++) {
+        navItem[j].className = '';
+    }
+    navItem[idx].className = 'current';
+    let left = idx * viewWidth;
+    slideContent.style.left = -left + 'px';
+    // 一秒钟后才可以进行下一次点击切换幻灯片
+    timer = setTimeout(function () {
+        flag = false;
+        clearTimeout(timer);
+    }, 1000);
 }
