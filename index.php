02533c597bc525112d297d4a75515568b9b9d858
马义雯 <402635876@qq.com>
2020-05-26 21:25:44 +0800

    从服务器获取列表

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" href="./css/main.css" />
   <!-- <link rel="stylesheet" href="./front/font-awesome-4.7.0/css/font-awesome.min.css"> -->
   <link rel="stylesheet" href="./front/iconfont/iconfont.css">
-  
+
   <title>郎朗口腔</title>
 </head>
 
@@ -185,13 +185,13 @@
     <div class="slide-content">
       <ul class="clearfix">
         <li>
-          <img src="./images/img.jpg" alt="朗朗口腔" width="100%"/>
+          <img src="./images/img.jpg" alt="朗朗口腔" width="100%" />
         </li>
         <li>
-          <img src="./images/img.jpg" alt="朗朗口腔" width="100%"/>
+          <img src="./images/img.jpg" alt="朗朗口腔" width="100%" />
         </li>
         <li>
-          <img src="./images/img.jpg" alt="朗朗口腔" width="100%"/>
+          <img src="./images/img.jpg" alt="朗朗口腔" width="100%" />
         </li>
       </ul>
     </div>
@@ -202,6 +202,40 @@
     </ul>
     <span class="slide-prev"><i class="iconfont icon-jiantou-copy"></i></span>
     <span class="slide-next"><i class="iconfont icon-jiantou"></i></span>
+  </div>
+  <!-- 内容区 -->
+  <div class="container">
+    <div class="main-column">
+      <h4>郎朗口腔</h4>
+      <p>呵护牙齿健康 创造美好未来<span></span>CARE FOR TEETH TO CREATE A BETTER FUTURE</p>
+    </div>
+    <div class="info-list">
+      <div id="infoListWrap">
+        <div class="loading-wait">
+          加载中
+          <div class="loading-icon"><i class="iconfont icon-jiazai"></i></div>
+        </div>
+      </div>
+      
+      <!-- <ul class="clearfix">
+        <figure>
+          <img src="./images/pic001.jpg" alt="郎朗口腔">
+          <figcaption>朗朗口腔</figcaption>
+          <p>诊所内设有典雅舒适、风格各异的独立诊疗室，如奇趣可爱的儿童诊室、适应女性需求的Hello Kitty...</p>
+        </figure>
+        <figure>
+          <img src="./images/pic001.jpg" alt="舒适的就诊环境">
+          <figcaption>舒适的就诊环境</figcaption>
+          <p>诊所内设有典雅舒适、风格各异的独立诊疗室，如奇趣可爱的儿童诊室、适应女性需求的...</p>
+        </figure>
+        <figure>
+          <img src="./images/pic001.jpg" alt="高端的诊疗设备">
+          <figcaption>高端的诊疗设备</figcaption>
+          <p>朗朗口腔拥有全球先进口腔诊疗技术，引进国外多款高端的齿科诊疗设备。包括：德国进口种植机...</p>
+        </figure>
+      </ul> -->
+      <a href="javascript:;" class="more-link">查看更多</a>
+    </div>
   </div>
   <!-- 页脚 -->
   <footer>
@@ -277,7 +311,56 @@
       <div id="copyright">© 2005 - 2016 朗朗口腔医疗投资有限公司 版权所有 粤ICP备08130115号-1</div>
     </div>
   </footer>
+  <script src="./js/jquery.min.js"></script>
   <script src="./js/banner.js"></script>
+  <script>
+    $.ajax({
+      url:"./data/index.php",
+      dataType:"json",
+      async:true,
+      cache:false,
+      data:{"categoryId":123456},
+      type:"POST",
+      beforeSend:function(){
+
+      },
+      success:function(req){
+        let infoListWrap=document.getElementById('infoListWrap');
+        if(!req.data||req.data.length===0){
+          infoListWrap.innerHTML=`<div class="loading-wait">
+                                    当前没有数据
+                                  </div>`;
+          return false;
+        }
+        let ul='<ul class="clearfix">';
+        let n=req.data.length;
+        console.log(n);
+        req.data.forEach(item=>{
+          
+          let img=item.imgUrl;
+          let title=item.title;
+          let dec=item.dec;
+          ul+=`<figure>
+                <a href="./data/newsDetail.php?id=${item.id}">
+                <img src="${img}" alt="${title}">
+                <figcaption>${title}</figcaption>
+                <p>${dec}</p>
+                </a>
+              </figure>`;
+        })
+        ul+='</ul>';
+        
+        infoListWrap.innerHTML=ul;
+      },
+      complete:function(){
+
+      },
+      error:function(){
+
+      }
+    });
+
+  </script>
 </body>
 
 </html>
