ad61cd8690202d312f67809a779b0f7f5139ab6f
马义雯 <402635876@qq.com>
2020-05-29 21:39:20 +0800

    优化细节

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
-function loadingMsg(params){
-    let wrap=document.getElementById('infoListWrap');
-    wrap.innerHTML=`<div class="loading-wait">
+function loadingMsg(params) {
+    let wrap = document.getElementById(params.wrap);
+    wrap.innerHTML = `<div class="loading-wait">
                     ${params.msg}
                     <div class="loading-icon"><i class="iconfont icon-jiazai"></i></div>
                 </div>`;
-    
+
 }
